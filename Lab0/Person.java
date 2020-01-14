cf9aa012cb49319c2cfa16cf9004e76c51eb459a
Anand Singh <42609735+Anand-S23@users.noreply.github.com>
2020-01-14 18:43:33 -0500

    Create Person.java

---
+++ Lab0/Person.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab0;
+
+/**
+ *
+ * @author tul01835
+ */
+public class Person {
+    private String name;
+    private int age;
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void incrementAge() {
+        age++;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " " + age;
+    }
+}
