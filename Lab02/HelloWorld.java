d1cbbbf301cff864d273fec70255c4430b2b0208
Anand Singh <42609735+Anand-S23@users.noreply.github.com>
2020-01-21 20:11:33 -0500

    Create HelloWorld.java

---
+++ Lab02/HelloWorld.java
@@ -0,0 +1,138 @@
+/*
+Author: Anand Singh 
+
+Task: Print Hello World
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author tul01835
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        printHelloWorld(); 
+    }
+    
+    // Prints H 
+    public static void printH()
+    {
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println("HHHHH");
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println("");
+    }
+    
+    // Prints E 
+    public static void printE()
+    {
+        System.out.println("EEEEE");
+        System.out.println("E");
+        System.out.println("EEEEE");
+        System.out.println("E");
+        System.out.println("EEEEE");
+        System.out.println("");
+    }
+    
+    // Prints L
+    public static void printL()
+    {
+        System.out.println("L");
+        System.out.println("L");
+        System.out.println("L");
+        System.out.println("L");
+        System.out.println("LLLLL");
+        System.out.println("");
+    }
+    
+    // Prints O
+    public static void printO()
+    {
+        System.out.println("OOOOO");
+        System.out.println("O   O");
+        System.out.println("O   O");
+        System.out.println("O   O");
+        System.out.println("OOOOO");
+        System.out.println("");
+    }
+    
+    // Prints comma
+    public static void printComma()
+    {
+        System.out.println("\n\n\n");
+        System.out.println(",,");
+        System.out.println(",");
+    }
+       
+    // Prints W
+    public static void printW()
+    {
+        System.out.println("W   WW   W");
+        System.out.println("W   WW   W");
+        System.out.println(" W W  W W");
+        System.out.println(" W W  W W");
+        System.out.println("  W    W ");
+        System.out.println("");
+    }
+    
+    // Prints R 
+    public static void printR()
+    {
+        System.out.println("RRRRR");
+        System.out.println("R   R");
+        System.out.println("RRRRR");
+        System.out.println("R R");
+        System.out.println("R   R");
+        System.out.println("");
+    }  
+    
+    // Prints D
+    public static void printD()
+    {
+        System.out.println("DDDD");
+        System.out.println("D    D");
+        System.out.println("D    D");
+        System.out.println("D    D");
+        System.out.println("D  D");
+        System.out.println("");
+    }
+    
+    // Prints Exclamation
+    public static void printExclamation()
+    {
+        System.out.println("!!");
+        System.out.println("!!");
+        System.out.println("!!");
+        System.out.println("");
+        System.out.println("!!");
+        System.out.println("");
+    }
+    
+    // Prints Hello World
+    public static void printHelloWorld()
+    {
+        printH();
+        printE();
+        printL();
+        printL();
+        printO();
+        printComma();
+        
+        printW();
+        printO();
+        printR();
+        printL();
+        printD();
+        printExclamation();
+    }
+}
